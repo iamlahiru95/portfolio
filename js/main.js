95b42c4111406c407c558f1e39afd85a7442610d
Lahiru Sandaruwan <lahirus250@gmail.com>
2025-02-10 20:40:34 +0530

    added an alert and integrate the JS script file

---
+++ js/main.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", () => {
+  alert(
+    "Dear user, Please not this website is under development. So there are not developed sections in the website. Apologize for any inconvenience.."
+  );
+});
